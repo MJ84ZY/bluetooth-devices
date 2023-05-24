40058d06860de21ff5ed257b6a4d0ba74fe1af53
ZYing <5281999zhiying@gmail.com>
2023-05-24 13:48:05 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+devices.onGamepadButton(MesDpadButtonInfo.BDown, function () {
+    basic.showArrow(ArrowNames.South)
+})
+devices.onGamepadButton(MesDpadButtonInfo.ADown, function () {
+    basic.showArrow(ArrowNames.North)
+})
+basic.forever(function () {
+	
+})
